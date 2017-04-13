3c41a7ed9dbcbbef39c9312921bd262c24bb485b
lambdac1739 <ec1739@laposte.net>
2017-04-13 21:07:51 +0200

    Val_block

--- src/byterun/values.h
+++ src/byterun/values.h
@@ -56,7 +56,7 @@ typedef int32_t  mlsize_t;
 #define Val_int(x)  (((val_t)(x) << 1 ) + 1)
 #define Int_val(x) ((x) >> 1)
 
-#define Val_block(x) ((val_t)(int16_t)x)
+#define Val_block(x) (((val_t)(int16_t)(val_t *)x) | 0x7f800000)
 #define Block_val(x) ((val_t *)(int16_t)x)
 
 /* Structure de l'entête : est-ce que la couleur est utile ?
