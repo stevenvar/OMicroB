688b19a7a4f04f7eefd6e741c2771d16324d39fd
Steven Varoumas <steven.varoumas@gmail.com>
2017-06-29 15:55:35 +0200

    added graphic primitives for arduboy

--- src/byterun/stdlib.c
+++ src/byterun/stdlib.c
@@ -60,6 +60,38 @@ val_t ocaml_arduboy_display(val_t unit) {
   return Val_unit;
 }
 
+val_t ocaml_arduboy_draw_rect(val_t x, val_t y, val_t w, val_t h, val_t color) {
+  arduboy.drawRect(Int_val(x),Int_val(y),Int_val(w),Int_val(h),Int_val(color));
+  return Val_unit;
+}
+
+val_t ocaml_arduboy_draw_circle(val_t x0, val_t y0, val_t r, val_t color) {
+  arduboy.drawCircle(Int_val(x0),Int_val(y0),Int_val(r),Int_val(color));
+  return Val_unit;
+}
+
+val_t ocaml_arduboy_draw_line(val_t x0, val_t y0, val_t x1, val_t y1, val_t color) {
+  arduboy.drawLine(Int_val(x0),Int_val(y0),Int_val(x1),Int_val(y1),Int_val(color));
+  return Val_unit;
+}
+
+val_t ocaml_arduboy_draw_pixel(val_t x, val_t y, val_t color) {
+  arduboy.drawPixel(Int_val(x),Int_val(y),Int_val(color));
+  return Val_unit;
+}
+
+
+val_t ocaml_arduboy_fill_rect(val_t x, val_t y, val_t w, val_t h, val_t color) {
+  arduboy.fillRect(Int_val(x),Int_val(y),Int_val(w),Int_val(h),Int_val(color));
+  return Val_unit;
+}
+
+val_t ocaml_arduboy_fill_circle(val_t x0, val_t y0, val_t r, val_t color) {
+  arduboy.fillCircle(Int_val(x0),Int_val(y0),Int_val(r),Int_val(color));
+  return Val_unit;
+}
+
+
 #else
 
 #include <stdio.h>
@@ -77,7 +109,7 @@ val_t ocaml_arduboy_print(val_t str) {
 }
 
 val_t ocaml_arduboy_print_int(val_t i) {
-  printf("ocaml_arduino_print_int(\"%d\")\n", Int_val(i));
+  printf("ocaml_arduino_print_int(\"%d\")\n", (int)Int_val(i));
   return Val_unit;
 }
 
@@ -85,6 +117,40 @@ val_t ocaml_arduboy_display(val_t unit) {
   printf("ocaml_arduino_print()\n");
   return Val_unit;
 }
+
+
+val_t ocaml_arduboy_draw_rect(val_t x, val_t y, val_t w, val_t h, val_t color) {
+  printf("drawRect(%d,%d,%d,%d,%d)", (int)Int_val(x), (int)Int_val(y),(int)Int_val(w),(int)Int_val(h),(int)Int_val(color));
+  return Val_unit;
+}
+
+val_t ocaml_arduboy_draw_circle(val_t x0, val_t y0, val_t r, val_t color) {
+  printf("drawCircle(%d,%d,%d,%d)", (int)Int_val(x0), (int)Int_val(y0),(int)Int_val(r),(int)Int_val(color));
+  return Val_unit;
+}
+
+val_t ocaml_arduboy_draw_line(val_t x0, val_t y0, val_t x1, val_t y1, val_t color) {
+   printf("drawLine(%d,%d,%d,%d,%d)", (int)Int_val(x0), (int)Int_val(y0),(int)Int_val(x1),(int)Int_val(y1),(int)Int_val(color));
+  return Val_unit;
+}
+
+val_t ocaml_arduboy_draw_pixel(val_t x, val_t y, val_t color) {
+   printf("drawPixel(%d,%d,%d)", (int)Int_val(x), (int)Int_val(y),(int)Int_val(color));
+  return Val_unit;
+}
+
+val_t ocaml_arduboy_fill_rect(val_t x, val_t y, val_t w, val_t h, val_t color) {
+  printf("fillRect(%d,%d,%d,%d,%d)", (int)Int_val(x), (int)Int_val(y),(int)Int_val(w),(int)Int_val(h),(int)Int_val(color));
+  return Val_unit;
+}
+
+val_t ocaml_arduboy_fill_circle(val_t x0, val_t y0, val_t r, val_t color) {
+  printf("fillCircle(%d,%d,%d,%d)", (int)Int_val(x0), (int)Int_val(y0),(int)Int_val(r),(int)Int_val(color));
+  return Val_unit;
+}
+
+
+
 
 #endif
 
--- tests/arduboy/blink.ml
+++ tests/arduboy/blink.ml
@@ -3,6 +3,12 @@ module Arduboy = struct
   external print_int : int -> unit = "ocaml_arduboy_print_int"
   external print   : string -> unit = "ocaml_arduboy_print"
   external display : unit   -> unit = "ocaml_arduboy_display"
+  external draw_rect : int -> int -> int -> int -> int -> unit = "ocaml_arduboy_draw_rect"
+  external draw_circle : int -> int -> int -> int -> unit = "ocaml_arduboy_draw_circle"
+  external draw_line : int -> int -> int -> int -> int -> unit = "ocaml_arduboy_draw_line"
+  external draw_pixel : int -> int -> int -> unit = "ocaml_arduboy_draw_pixel"
+  external fill_rect : int -> int -> int -> int -> int -> unit = "ocaml_arduboy_fill_rect"
+  external fill_circle : int -> int -> int -> int -> unit = "ocaml_arduboy_fill_circle"
 end
 
 let rec facto x =
@@ -21,15 +27,21 @@ let rec facto x =
 
 let () =
   Arduboy.init ();
-  for i = 0 to 10 do
-  (* Arduboy.print "facto "; *)
-  (* Arduboy.print_int i; *)
-  (* Arduboy.print " = "; *)
-    let x = facto i in
-    (* x *)
-  Arduboy.print_int (x);
-  Arduboy.print " \n";
+  (* for i = 0 to 4 do *)
+  (* (\* Arduboy.print "facto "; *\) *)
+  (* (\* Arduboy.print_int i; *\) *)
+  (* (\* Arduboy.print " = "; *\) *)
+  (*   let x = facto i in *)
+  (*   (\* x *\) *)
+  (* Arduboy.print_int (x); *)
+  (* Arduboy.print " \n"; *)
+  (* Arduboy.display (); *)
+  (* (\* delay 1000 *\) *)
+  (* (\* blink PIN17 100 1000 *\) *)
+  (* done; *)
+  for i = 0 to 5 do
+    Arduboy.draw_circle 64 32 (i*10) 255;
+    Arduboy.draw_line 0 (i*10) 128 (i*10) 100;
+  done;
+  Arduboy.fill_rect 12 12 22 22 100;
   Arduboy.display ();
-  (* delay 1000 *)
-  (* blink PIN17 100 1000 *)
-done
