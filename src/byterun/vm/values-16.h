ff3ae33ff1e8ad6e8b8e3bc83f4e9eb421b7151a
Steven Varoumas <steven.varoumas@gmail.com>
2018-02-12 14:18:28 +0100

    fix warning by adding some explicit cast

--- src/byterun/vm/values-16.h
+++ src/byterun/vm/values-16.h
@@ -91,9 +91,9 @@ typedef uint32_t code_t;
 /******************************************************************************/
 /* Conversions */
 
-#define Val_dynamic_block(x) ((value) ((char *) (x) - (char *) ocaml_ram_heap) << 1)
-#define Val_static_block(x)  ((value) ((char *) (x) - (char *) ocaml_ram_heap) << 1)
-#define Val_flash_block(x)   ((value) ((char *) (x) - (char *) ocaml_flash_heap + (ROUND_RAM_WOSIZE << 1)) << 1)
+#define Val_dynamic_block(x) (value)((value) ((char *) (x) - (char *) ocaml_ram_heap) << 1)
+#define Val_static_block(x)  (value)((value) ((char *) (x) - (char *) ocaml_ram_heap) << 1)
+#define Val_flash_block(x)   (value)((value) ((char *) (x) - (char *) ocaml_flash_heap + (ROUND_RAM_WOSIZE << 1)) << 1)
 
 #define Ram_block_val(x)     ((value *) ((char *) ocaml_ram_heap + ((value) (x) >> 1)))
 #define Flash_block_val(x)   ((value *) ((char *) ocaml_flash_heap + ((uvalue) (x) >> 1) - (ROUND_RAM_WOSIZE << 1)))
