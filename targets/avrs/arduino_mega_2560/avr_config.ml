6ee2ed0c5c6b6725c62d40f3d887eec38723f742
Vertmo <basile.pesin@gmail.com>
2019-01-24 20:57:12 +0100

    Preparing for a more generic version

--- targets/avrs/arduino_mega_2560/avr_config.ml
+++
@@ -1,4 +0,0 @@
-let default_mmcu  = "atmega2560"
-let default_avr   = "avrispmkii"
-let default_baud  = 115_200
-let default_clock = 16_000_000
--- targets/avrs/arduino_uno/avr_config.ml
+++
@@ -1,4 +0,0 @@
-let default_mmcu  = "atmega328p"
-let default_avr   = "arduino"
-let default_baud  = 115_200
-let default_clock = 16_000_000
