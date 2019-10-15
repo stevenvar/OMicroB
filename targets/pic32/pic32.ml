4cd60ca93ca91b040f1f700539cdf8f44ec100a1
Adilla Susungi <adilla.susungi@gmail.com>
2019-10-15 11:40:30 +0200

    Added pic32.ml and pic32.mli.

---
+++ targets/pic32/pic32.ml
@@ -0,0 +1,45 @@
+(*******************************************************************************)
+(*                                                                             *)
+(*                  Generic PIC32 pin communication library                    *)
+(*                                                                             *)
+(*******************************************************************************)
+
+
+
+type level = HIGH | LOW
+type mode = INTPUT | OUTPUT
+
+type yes
+type no
+type 'a analog_pin = YES : yes analog_pin | NO : no analog_pin
+
+module type Pic32Pins = sig
+  type ('a, 'b, 'c, 'd) pin
+  type 'a register
+  val lat_of_pin :
+    ('a register, 'b register, 'c register, 'd analog_pin) pin -> 'a register
+  val tris_of_pin :
+    ('a register, 'b register, 'c register, 'd analog_pin) pin -> 'b register
+  val port_of_pin :
+    ('a register, 'b register, 'c register, 'd analog_pin) pin -> 'c register
+  val lat_bit_of_pin :
+    ('a register, 'b register, 'c register, 'd analog_pin) pin -> 'a
+  val tris_bit_of_pin :
+    ('a register, 'b register, 'c register, 'd analog_pin) pin -> 'b
+  val port_bit_of_pin :
+    ('a register, 'b register, 'c register, 'd analog_pin) pin -> 'c
+  val pin_mode : 
+    ('a register, 'b register, 'c register, 'd analog_pin) pin -> mode -> unit
+  val digital_write :
+    ('a register, 'b register, 'c register, 'd analog_pin) pin -> level -> unit
+  val digital_read :
+    ('a register, 'b register, 'c register, 'd analog_pin) pin -> level
+  val write_register : 'a register -> int -> unit
+  val read_register : 'a register -> int 
+  val set_bit : 'a register -> 'a -> unit
+  val clear_bit : 'a register -> 'a -> unit
+  val read_bit : 'a register -> 'a -> bool
+end
+
+external delay : int -> unit = "caml_pic32_delay" [@@noalloc]
+
