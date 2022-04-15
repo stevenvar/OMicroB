dccc3dbc94d7d1fa6e2cf43cc5cc6c844cdb5d64
Basile Pesin <basile.pesin@vertmo.org>
2022-04-15 16:44:41 +0200

    More features on arduboy pins

--- targets/avr/arduboy/arduboyPins.mli
+++ targets/avr/arduboy/arduboyPins.mli
@@ -10,20 +10,20 @@ type 'a pin =
   | PIN4  : [< `OLED_DC ] pin
   | PIN5  : [< `SPEAKER_PLUS ] pin
   | PIN6  : [< `OLED_RST ] pin
-  | PIN7  : [< `BUTTON_A ] pin
-  | PIN8  : [< `BUTTON_B ] pin
-  | PIN9  : [< `LED_BLUE ] pin
-  | PIN10 : [< `LED_RED ] pin
+  | PIN7  : [< `BUTTON_A | `DREAD ] pin
+  | PIN8  : [< `BUTTON_B | `DREAD ] pin
+  | PIN9  : [< `LED_BLUE | `DWRITE ] pin
+  | PIN10 : [< `LED_RED | `DWRITE ] pin
   | PIN12 : [< `OLED_CS ] pin
-  | PIN11 : [< `LED_GREEN ] pin
+  | PIN11 : [< `LED_GREEN | `DWRITE ] pin
   | PIN13 : [< `SPEAKER_MINUS ] pin
   | MISO  : [< `MISO ] pin
   | SCK   : [< `SCK ] pin
   | MOSI  : [< `MOSI ] pin
-  | PINA0 : [< `BUTTON_UP ] pin
-  | PINA1 : [< `BUTTON_RIGHT ] pin
-  | PINA2 : [< `BUTTON_LEFT ] pin
-  | PINA3 : [< `BUTTON_DOWN ] pin
+  | PINA0 : [< `BUTTON_UP | `DREAD ] pin
+  | PINA1 : [< `BUTTON_RIGHT | `DREAD ] pin
+  | PINA2 : [< `BUTTON_LEFT | `DREAD ] pin
+  | PINA3 : [< `BUTTON_DOWN | `DREAD ] pin
 include AvrPins
   with type register := register
   with type 'a pin := 'a pin
