5967a692d2a85772cecc3252ece694ae00d6f38f
Basile Pesin <basile.pesin@vertmo.org>
2022-04-16 16:18:10 +0200

    More corrections on arduboy and factorize / fixed some examples
    gitignore for a test

--- tests/tuto_4_snake/spi.ml
+++ tests/tuto_4_snake/spi.ml
@@ -3,16 +3,24 @@ open Avr
 (* Initialise SPI connection *)
 let begin_spi ~ss ~sck ~mosi =
   pin_mode ss OUTPUT;
-  set_bit SPCR MSTR;
-  set_bit SPCR SPE;
+  set_bit SPCR BIT4;
+  set_bit SPCR BIT6;
   pin_mode sck OUTPUT;
   pin_mode mosi OUTPUT
+  (* mettre ss en sortie *)
+  (* passer l'arduboy en mode maître *)
+  (* activer le SPI *)
+  (* mettre sck et mosi en sortie *)
 
 (* Close SPI connection *)
-let end_spi () =
-  clear_bit SPCR SPE
+  let end_spi () =
+    clear_bit SPCR BIT6
+(* desactiver le SPI *)
 
 (* Emit data through the SPI connection *)
 let transfer data =
   write_register SPDR data;
-  while (read_bit SPSR SPIF <> true) do () done
+  while (not (read_bit SPSR BIT7)) do () done
+  (* écrire dans le registre SPDR *)
+  (* attendre que SPSR[SPIF] vaille 1 *)
+
