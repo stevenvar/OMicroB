d28207846554f562a9cec9a73b6d07c61eaf3ab1
Basile Pesin <basile.pesin@vertmo.org>
2024-10-23 12:44:00 +0200

    Using nano.specs?

--- targets/numworks/device_config.ml
+++ targets/numworks/device_config.ml
@@ -45,7 +45,7 @@ let compile_c_to_hex ~local ~trace:_ ~verbose input output =
   let cmd = [ Config.arm_cxx ] @ default_arm_cxx_options in
   let cmd = cmd @ [ "-Wl,--relocatable" ] in
   let cmd = cmd @ [ "-nostartfiles" ] in
-  let cmd = cmd @ [ "--specs=nosys.specs" ] in
+  let cmd = cmd @ [ "--specs=nano.specs" ] in
   let cmd = cmd @ [ "-fdata-sections"; "-ffunction-sections" ] in
   let cmd = cmd @ [ "-Wl,-e,__start"; "-Wl,-u,eadk_app_name"; "-Wl,-u,eadk_app_icon"; "-Wl,-u,eadk_api_level" ] in
   let cmd = cmd @ [ "-Wl,--gc-sections" ] in
